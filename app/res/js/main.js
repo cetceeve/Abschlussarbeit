f57c7c057be4fca5c94bf491c5af0570154d2c12
Fabian Zeiher <fzeiher@gmail.com>
2020-09-29 14:27:39 +0200

    add eslint config file

--- app/res/js/main.js
+++ app/res/js/main.js
@@ -1,19 +1,21 @@
+/* global CodeMirror */
+
 const currentUser = {
-    id: 123,
-    avatarUrl: "https://pbs.twimg.com/profile_images/964204609186222081/I7Mc16_z.jpg",
-    name: "Seoulbear"
-}
+  id: 123,
+  avatarUrl: "https://pbs.twimg.com/profile_images/964204609186222081/I7Mc16_z.jpg",
+  name: "Seoulbear",
+};
 
 function init() {
-    console.log("javascript initialized");
-    var SideComments = require("side-comments"),
-        existingComments = JSON.parse(document.querySelector("#existing-comments").innerHTML),
-        myCodeMirror = CodeMirror(document.body, {
-            value: "function myScript(){return 100;}\n",
-            mode: "javascript"
-        });
+  // eslint-disable-next-line no-undef
+  var SideComments = require("side-comments"),
+    existingComments = JSON.parse(document.querySelector("#existing-comments").innerHTML),
+    myCodeMirror = CodeMirror(document.body, {
+      value: "function myScript(){return 100;}\n",
+      mode: "javascript",
+    });
 
-    let sideComments = new SideComments("#commentable-area", currentUser, existingComments);
+  let sideComments = new SideComments("#commentable-area", currentUser, existingComments);
 }
 
 init();
