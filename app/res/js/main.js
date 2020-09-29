a8eddd4d0b758c488cbd86889eeff5635bc171f0
Fabian Zeiher <fzeiher@gmail.com>
2020-09-29 13:51:59 +0200

    add static side-comments

--- app/res/js/main.js
+++ app/res/js/main.js
@@ -1,5 +1,18 @@
+const currentUser = {
+    id: 123,
+    avatarUrl: "https://images6.fanpop.com/image/photos/42600000/LOONA-X-X-photoshoot-behind-Hyunjin-loo-CE-A0-CE-94-42662157-840-1260.jpg",
+    name: "Hyunjin"
+}
+
 function init() {
     console.log("javascript initialized");
+    var SideComments = require("side-comments"),
+        existingComments = JSON.parse(document.querySelector("#existing-comments").innerHTML);
+
+    console.log(SideComments);
+    console.log(existingComments);
+
+    sideComments = new SideComments("#commentable-area", currentUser, existingComments);
 }
 
 init();
