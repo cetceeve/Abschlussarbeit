68b5384a37c445b0508c95ec9c4d1f7d7eabe573
Fabian Zeiher <fzeiher@gmail.com>
2020-09-30 22:33:26 +0200

    created site with side-comments

--- app/res/js/VueApp.js
+++ app/res/js/VueApp.js
@@ -1,14 +1,36 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
+import DataService from "./services/side-comment-data.js";
 
 var vue = new Vue({
     el: "#app",
     data: {
-        header: "HI, welcome to the store!",
+        header: "Please, enjoy the vue!",
+        currentUser: {
+            id: 123,
+            avatarUrl: "https://pbs.twimg.com/profile_images/964204609186222081/I7Mc16_z.jpg",
+            name: "Seoulbear",
+        },
     },
     computed: {
 
     },
     methods: {
-
+        handleCommentPosted: function (comment) {
+            let dataService = new DataService();
+            dataService.addEventListener("commentSaved", () => {
+                this.sideComments.insertComment(comment);
+            });
+            dataService.saveComment(comment);
+        },
+    },
+    mounted: function () {
+        let dataService = new DataService();
+        dataService.addEventListener("commentsLoaded", event => {
+            // eslint-disable-next-line no-undef
+            let SideComments = require("side-comments");
+            this.sideComments = new SideComments("#commentable-area", this.currentUser, event.data.allComments);
+            this.sideComments.on("commentPosted", comment => this.handleCommentPosted(comment));
+        });
+        dataService.getComments();
     },
 });
