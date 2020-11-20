75fece4c8b1aa59f4d14b050561122e0601fe1d3
Fabian Zeiher <fzeiher@gmail.com>
2020-11-20 12:37:14 +0100

    cleaning out the project

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
 /* eslint-env node */
 
-const AppServer = require("./lib/AppServer.js"),
-  fs = require("fs");
+const express = require("express"),
+fs = require("fs"),
+path = require("path"),
+state = JSON.parse(fs.readFileSync("./data/test_state.json", "utf8")),
+port = 3000;
 
-var server,
-  state = JSON.parse(fs.readFileSync("./data/test_state.json", "utf8"));
+let app = express();
+app.use(express.json());
+app.use("/res", express.static("./app/res"));
+app.use("/vendors", express.static("./app/vendors"));
 
-/**
- * Starts webserver to serve files from "/app" folder
- */
-function init() {
-  // Access command line parameters from start command (see package.json)
-  let appDirectory = process.argv[2], // folder with client files
-    appPort = process.argv[3]; // port to use for serving static files
-  server = new AppServer(appDirectory);
-  server.start(appPort);
-}
-
-init();
-
-server.app.get("/", (req, res) => {
-  console.log(req.query);
-  console.log("did this work?");
+app.listen(port, function () {
+    console.log(`AppServer started. Client available at http://localhost:${port}`);
 });
 
-server.app.post("/", (req, res) => {
-  console.log("Endpoint /");
+app.get("/", (req, res) => {
+    console.log("did this work?");
+    res.sendFile(path.join(__dirname + "/app/index.html"));
 });
 
-server.app.post("/editor", (req, res) => {
-  console.log("/editor: sending");
-  let fileString = fs.readFileSync("./lib/AppServer.js", "utf8");
-  res.json({
-    sha: "fileSha0000",
-    value: fileString,
-  });
+app.get("/review-editor", (req, res) => {
+    console.log(req.query);
+    res.sendFile(path.join(__dirname + "/app/review-editor.html"));
 });
