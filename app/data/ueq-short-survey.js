812ffe4dea3e1ee9e71fad4c614303077466b54c
Fabian Zeiher <fzeiher@gmail.com>
2020-11-23 17:36:25 +0100

    moved ueq survey object to seperate file

---
+++ app/data/ueq-short-survey.js
@@ -0,0 +1,50 @@
+let ueqs = {
+    name: "UEQ-S",
+    title: "Bitte bewerte die Anwendung!",
+    description: "Entscheide so spontan wie möglich welcher der folgenden widersprüchlichen Terme die Anwendung besser beschreibt. Es gibt keine richtigen oder falschen Antworten. Nur deine Meinung zählt.",
+    likertRange: "7",
+    items: [
+        {
+            leftLabel: "behindernd",
+            rightLabel: "unterstützend",
+            value: null,
+        },
+        {
+            leftLabel: "kompliziert",
+            rightLabel: "einfach",
+            value: null,
+        },
+        {
+            leftLabel: "ineffizient",
+            rightLabel: "effizient",
+            value: null,
+        },
+        {
+            leftLabel: "verwirrend",
+            rightLabel: "übersichtlich",
+            value: null,
+        },
+        {
+            leftLabel: "langweilig",
+            rightLabel: "spannend",
+            value: null,
+        },
+        {
+            leftLabel: "uninteressant",
+            rightLabel: "interessant",
+            value: null,
+        },
+        {
+            leftLabel: "konventionell",
+            rightLabel: "originell",
+            value: null,
+        },
+        {
+            leftLabel: "herkömmlich",
+            rightLabel: "neuartig",
+            value: null,
+        },
+    ],
+};
+
+export default ueqs;
