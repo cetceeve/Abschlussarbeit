7044d8554a29629c838a8be80205a3ba769812d4
Fabian Zeiher <fzeiher@gmail.com>
2020-11-27 11:43:12 +0100

    simplify ip creation

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
-const { v4: uuidv4 } = require("uuid"),
-crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
 
 class ServerUtils {
     constructor(server, sql, redis) {
@@ -8,39 +7,20 @@ class ServerUtils {
         this.sql = sql;
         this.redis = redis;
     }
-
+    
     sessionIdMiddleware() {
         return (req, res, next) => {
             // check if client sent session id cookie
             if (req.cookies.sessionId === undefined) {
-                // disguise user ip as hash for data privacy
-                let ipHash = crypto.createHash("sha1").update(req.ip).digest("hex");
-                // check if ip was already, if true, reasign old sessionId
-                // this is a mechanism against fraud
-                this.redis.get(ipHash, (err, reply) => {
-                    if (reply === null) {
-                        // this ip was not seen before, create a new sessionId and store it as a cookie
-                        let newSessionId = uuidv4();
-                        res.cookie("sessionId", newSessionId, { 
-                            expires: new Date(Date.now() + 1000 /*sec*/ * 60 /*min*/ * 60 /*hour*/ * 24 /*day*/ * 30),
-                            httpOnly: true,
-                        });
-                        // save the ip and sessionId for 23 hours
-                        this.redis.set(ipHash, newSessionId);
-                        this.redis.expire(ipHash, 60*60*24);
-                        // register the new session
-                        this.redis.sadd("sessions", newSessionId);
-                        this.sql.registerSession(newSessionId);
-                        console.log("SERVER: Cookie created successfully");
-                    } else {
-                        // user was already seen, reasign old sessionId
-                        res.cookie("sessionId", reply, { 
-                            expires: new Date(Date.now() + 1000 /*sec*/ * 60 /*min*/ * 60 /*hour*/ * 24 /*day*/ * 30),
-                            httpOnly: true,
-                        });
-                        console.log("SERVER: Fraud attempt blocked");
-                    }
+                let newSessionId = uuidv4();
+                res.cookie("sessionId", newSessionId, { 
+                    expires: new Date(Date.now() + 1000 /*sec*/ * 60 /*min*/ * 60 /*hour*/ * 24 /*day*/ * 30),
+                    httpOnly: true,
                 });
+                // register the new session
+                this.redis.sadd("sessions", newSessionId);
+                this.sql.registerSession(newSessionId);
+                console.log("SERVER: Cookie created successfully");
             }
             next();
         };
